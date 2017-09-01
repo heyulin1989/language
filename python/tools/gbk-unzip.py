e51f967d87b3f5c8a78b780a9d26f5665f11876d
heyulin19890 <heyulin_1989@163.com>
2017-09-01 16:42:33 +0800

    使用pep8检查格式,修改

--- python/tools/gbk-unzip.py
+++ python/tools/gbk-unzip.py
@@ -8,12 +8,12 @@ import zipfile
 
 print "Processing File " + sys.argv[1]
 
-file=zipfile.ZipFile(sys.argv[1],"r");
+file = zipfile.ZipFile(sys.argv[1], "r");
 for name in file.namelist():
     utf8name=name.decode('gbk')
     print "Extracting " + utf8name
     pathname = os.path.dirname(utf8name)
-    if not os.path.exists(pathname) and pathname!= "":
+    if not os.path.exists(pathname) and pathname != "":
         os.makedirs(pathname)
     data = file.read(name)
     if not os.path.exists(utf8name):
