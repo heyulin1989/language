5497e38267f9ba675ccdf6b8273f01e9da77c23d
heyulin19890 <heyulin_1989@163.com>
2017-09-26 18:37:26 +0800

    添加注释

--- python/books/writing-solid-python-code-91-suggestions/23.py
+++ python/books/writing-solid-python-code-91-suggestions/23.py
@@ -11,9 +11,22 @@ def print_prime(n):
 def print_prime2(n):
 	for i in xrange(2, n):
 		for j in xrange(2, i):
+			print "i = {}, j = {}".format(i,j)
 			if i % j == 0:
 				break
 		else:
 			print '%d is a prime number' % i
 
-print print_prime2(20)
+
+
+print print_prime2(10)
+"""
+# while else 和for else  当有break时，这个else就会被执行
+i = 0
+while (i < 10):
+	i += 1
+	if i == 0:
+		break
+else:
+	print "ok"
+"""
