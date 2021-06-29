e06d5e85cbbe56052b3b081af526e12c88b8480a
user.email <heyulin_1989@163.com>
2021-06-29 08:32:41 +0800

    mxcsr

---
+++ asm/Beginning-x64-Assembly-Programming/chapter27/print_mxcsr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_mxcsr(long long n){
+    long long s,c;
+    for (c = 15; c >= 0; c--){
+        s = n >> c;
+        if ((c+1) % 4 == 0) printf(" ");
+
+        if (s & 1)
+            printf("1");
+        else
+            printf("0");
+    }
+    printf("\n");
+}
